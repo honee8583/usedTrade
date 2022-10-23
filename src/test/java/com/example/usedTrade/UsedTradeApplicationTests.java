75ecc3a47878e2376a988fbf57a660b83fe992d8
leedahoon <honee8583@naver.com>
2022-10-23 15:27:23 +0900

    Initial commit

---
+++ src/test/java/com/example/usedTrade/UsedTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usedTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
