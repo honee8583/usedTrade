5b4e172214e88c422f892466f6327c2db962ed8b
leedahoon <honee8583@naver.com>
2022-11-02 09:23:48 +0900

    OAuth2 구현

---
+++ src/main/java/com/example/usedTrade/security/config/PrincipalDetails.java
@@ -0,0 +1,46 @@
+package com.example.usedTrade.security.config;
+
+import com.example.usedTrade.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class PrincipalDetails extends User implements OAuth2User{
+
+    private String email;
+    private String password;
+    private String name;
+    private Map<String, Object> attr;
+
+    // Normal Login
+    public PrincipalDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.email = username;
+        this.password = password;
+    }
+
+    // OAuth2Login
+    public PrincipalDetails(String username, String password, Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attr) {
+        this(username, password, authorities);
+        this.attr = attr;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
+    }
+}
