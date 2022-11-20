12dec69ef7b71fbd8ad6e88bdcdf03056b08ba56
leedahoon <honee8583@naver.com>
2022-11-20 20:30:01 +0900

    관리자 메인화면 구현

--- src/main/java/com/example/usedTrade/main/MainController.java
+++ src/main/java/com/example/usedTrade/main/MainController.java
@@ -1,6 +1,7 @@
 package com.example.usedTrade.main;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +18,11 @@ public class MainController {
     @RequestMapping("/error/denied")
     public String error() {
         return "error/denied";
+    }
+
+    @GetMapping("/main.do")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminIndex() {
+        return "admin/index";
     }
 }
